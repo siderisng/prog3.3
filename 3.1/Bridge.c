9f07e067eea136eaa188d1d862a073e279d975a3
Fotis <fotanftm@gmail.com>
2014-11-12 18:19:06 +0200

    SUCCESS BIATCHEZZZZ

--- 3.1/Bridge.c
+++ 3.1/Bridge.c
@@ -13,8 +13,9 @@ void ExitCar (char *CarDir);
 void * Car(void* carDir);
 
 char currDir;
-pthread_mutex_t leftDir, rightDir, mtxCheck, mtxExit, mtxend;
-int countl, countr, flag;
+pthread_mutex_t mtxEnter, mtxExit, mtxend;
+pthread_cond_t entBridge;
+int count1, count2,countl, countr, flag, count, countlall,countrall, countl2, countr2;
 int carsPassed, carsleft, flag,waiting,startflag;
 
 
@@ -23,21 +24,33 @@ int main (int argc, char *argv[]){
 	int i,thrCheck;
 	pthread_t *p_thrd;
 	
+	
+	
+	currDir='r';
+	
 	flag=0;
 	startflag=0;
+	count1=0; count2=Capacity;
 	countl=0, countr=0;
 	carsPassed= Capacity;
 	carsleft=0;
 	printf ("Please type the directions of the cars. l for left r for right:");
 	scanf ("%s", directions);			//pairnoume eisodo gia ta aytokinita (l gia aristera, r gia deksia)
 	printf ("\n");
+	count=strlen(directions);
+	countrall=0;
+	countlall=0;
+	countr2=0;
+	countl2=0;
 	
 	for (i=0; i< strlen(directions); i++){
 		if (directions[i]=='r'){
-			countr++;			//afksanoume to countr gia kathe amaksi apo deksia
+			countrall++;
+			countr2++;//afksanoume to countr gia kathe amaksi apo deksia
 		}
 		else if(directions[i]=='l'){		//afksanoume to countl gia kathe amaksi apo aristera
-			countl++;
+			countlall++;
+			countl2++;
 		}
 	}
 	
@@ -47,6 +60,14 @@ int main (int argc, char *argv[]){
         return 1;
     }
 	
+	if (pthread_cond_init(&entBridge, NULL) != 0)	//arxikopoihsh mutex mtxExit
+    {
+        perror ("Condition error");
+        return 1;
+    }
+	
+	
+	
 	if (pthread_mutex_init(&mtxend, NULL) != 0)	//arxikopoihsh mutex mtxend kai kleidwma
     {
         perror ("Mutex error");
@@ -54,25 +75,7 @@ int main (int argc, char *argv[]){
     }
 	
 	pthread_mutex_lock (&mtxend);
-	if (pthread_mutex_init(&rightDir, NULL) != 0)	//arxikopoihsh mutex rightDir
-    {
-        perror ("Mutex error");
-        return 1;
-    }
-	
-	if (pthread_mutex_init(&leftDir, NULL) != 0)	//arxikopoihsh mutex leftDir
-    {
-        perror ("Mutex error");
-        return 1;
-    }
-	
-	if (pthread_mutex_init(&mtxCheck, NULL) != 0)	//arxikopoihsh mutex mtxCheck
-    {
-        perror ("Mutex error");
-        return 1;
-    }
-	
-	
+
 	
 	if (NULL==(p_thrd= (pthread_t*)malloc(strlen(directions)*sizeof(pthread_t)))){	//desmefsi xwrou gia ton pinaka apo threads
 		perror ("Memory allocation error");
@@ -89,10 +92,8 @@ int main (int argc, char *argv[]){
 	
 	
 	
-	 pthread_mutex_destroy(&mtxend);	//diagrafi mutexes
-	 pthread_mutex_destroy(&leftDir);
-	pthread_mutex_destroy(&rightDir);
-	pthread_mutex_destroy(&mtxCheck);
+	pthread_mutex_destroy(&mtxend);	//diagrafi mutexes
+	pthread_mutex_destroy(&mtxEnter);
 	pthread_mutex_destroy(&mtxExit);
 	free (p_thrd);				//apeleftherwsi xwrou gia ton pinaka apo threads
 	return 0;
@@ -100,82 +101,117 @@ int main (int argc, char *argv[]){
 
 
 void EnterCar(char *CarDir){
+	int chFlag=0;
 	
-	carsPassed--;				// +1 amaksi sti gefira
-	printf ("%c car on bridge\n",*CarDir);
-	if ((carsPassed!=0)&&(currDir=='l')){
-		pthread_mutex_unlock (&leftDir);	//an den einai gemati i gefira kai i katefthinsi einai aristera kseblokaroume ena amaksi apo aristera
-	}
-	else if ((carsPassed!=0)&&(currDir=='r')){	//to antistoixo gia amaksia apo deksia
-		pthread_mutex_unlock (&rightDir);
+	pthread_mutex_lock (&mtxEnter);		//mono ena thread bainei kathe fora sto krisimo simeio 
+		if (*CarDir=='r'){
+			countrall--;
+			
+		}
+		if (*CarDir=='l'){
+			countlall--;
+			
+		}
+	while ((*CarDir!=currDir)||(count1== Capacity)){
+		
+		
+		
+		if ((*CarDir=='r')&&(chFlag==0)){
+			countr++;
+			chFlag=1;
+		}
+		if ((*CarDir=='l')&&(chFlag==0)){
+			countl++;
+			chFlag=1;
+		}
+		pthread_cond_wait (&entBridge, &mtxEnter);
+		if (currDir!=*CarDir){
+			
+			pthread_cond_signal (&entBridge);
+		}
+		
 		
 	}
+	
+	
+	if (*CarDir=='r'){
+			countr2--;
+			
+		}
+	if (*CarDir=='l'){
+			countl2--;
+			
+		}
+	
+	
+	printf ("Enter %c direction\n", *CarDir);
+	count1++;
+	if ((currDir=='r')&&(countr!=0)&&(count1!=Capacity)){
+		countr--;
+			
+		pthread_cond_signal (&entBridge);
+	
+	}else if ((currDir=='l')&&(countl!=0)&&(count1!=Capacity)){
+		countl--;
+		pthread_cond_signal (&entBridge);
+		
+	}
+	
+	
+	
+	pthread_mutex_unlock (&mtxEnter);
 	
 }
 
 void ExitCar(char *CarDir){
 	pthread_mutex_lock (&mtxExit);		//mono ena thread bainei kathe fora sto krisimo simeio 
-	printf ("%c car leaving bridge\n", *CarDir);
-	carsleft++;				// +1 amaksi stin gefyra
 	
-	if (*CarDir=='l'){			// an erxetai apo aristera h deksia to amaksi afairoume apo ton antistoixo counter
-		countl--;}
-	if (*CarDir=='r'){
-		countr--;
-	}
-	if (countl==0){				//an den exei alla amaksia apo aristera kanoume tin katefthinsi deksia kai kseblokaroume amaksi apo deksia
-		
-		currDir='r';
-		flag=1;
-		
-	}
 	
-	if (countr==0){				//an den exei alla amaksia apo deksia kanoume tin katefthinsi aristera kai kseblokaroume amaksi apo aristera
-		
-		currDir='l';
-		flag=1;
-	}
 	
-	if (flag==0){
-		if ((carsleft==Capacity)&&(currDir=='r')){	//an exei gemisei i gefyra kai i diefthinsi einai apo deksia adeiazoume tin gefyra kai allazoume tin katefthinsi an exei enapomeinanta amaksia apo aristera
-		
-		carsleft=0;
-		carsPassed= Capacity;
-		
+	count2--;
+	printf ("Exit %c direction\n", *CarDir);
+	
+	if ((countr2==0)||(countl2==0)){
 		
 		
-		
-		if (countl>0){
-			currDir='l';
-			pthread_mutex_unlock(&leftDir);}
-		}
-		if ((carsleft==Capacity)&&(currDir=='l')){	//antistoixa me panw
-		
-		carsleft=0;
-		carsPassed= Capacity;
-		
-		
-		
-		if (countr>0){
+		if ((countl2)==0){
 			currDir='r';
-			pthread_mutex_unlock(&rightDir);
 		}
-		
+		else if (countr2==0){
+			currDir='l';
 		}
-		
-	}else {
-		if (carsleft==Capacity){
-			carsleft=0;
-			carsPassed= Capacity;
-		}
-		if ((carsleft!=Capacity)&&(currDir=='r')){
-			pthread_mutex_unlock(&rightDir);
-		}else if((currDir=='l')&&(carsleft!=Capacity)){
-			pthread_mutex_unlock(&leftDir);
-		}
-		
-		
+		if (count2==0){
+			printf ("\n\n");
+			count1=0;
+			count2=Capacity;}
+		pthread_cond_signal (&entBridge);
 	}
+	else if (count2==0){
+		
+		printf ("\n\n");
+		
+		count2=Capacity;
+		count1=0;
+		if (currDir=='r'){
+			currDir='l';
+		}
+		else if (currDir=='l'){
+			currDir='r';
+		}
+		printf ("%d\n", countrall);
+		
+		if ((countrall==0)&&(currDir=='r')){
+			flag=1;
+			pthread_cond_signal (&entBridge);
+		}
+		if ((countlall==0)&&(currDir=='l')){
+			flag=1;
+			pthread_cond_signal (&entBridge);
+		}
+	}
+
+	
+	
 	pthread_mutex_unlock (&mtxExit);		//kseblockarisma gia to epomeno thread 
 }
 
@@ -186,39 +222,11 @@ void * Car(void* carDir){
 	char * CarDir;
 	CarDir =(char*)carDir;	
 
-	pthread_mutex_lock (&mtxCheck);			//krisimo tmima
-	if (startflag==0){
-			
-			//to prwto amaksi orizei tin arxiki katefthinsi
-			if ('r'== *CarDir){			
-				currDir='r';
-				pthread_mutex_lock (&leftDir);
-			}
-			else if ('l'== *CarDir){
-				currDir='l';
-				pthread_mutex_lock (&rightDir);
-			}
-			startflag=1;
-			
-	}
-	pthread_mutex_unlock (&mtxCheck);
-	
-	//blokarisma twn amaksiwn stin antistoixi oura deksia h aristera
-	if ('r'== *CarDir){
-		pthread_mutex_lock (&rightDir);		//an kseblokarei kai exei ti swsti katefthinsi mpainei stin gefyra
-	}
-	if ('l'== *CarDir){
-		pthread_mutex_lock(&leftDir);
-		
-	}
+	EnterCar(CarDir);
 	
 	
-	EnterCar (CarDir);
+	ExitCar(CarDir);
 	
-	ExitCar (CarDir);
-	if ((countl==0) && (countr==0)){
-		pthread_mutex_unlock (&mtxend);		//an teleiwsoun ta amaksia termatizei kai to programa
-	}
 	
 	return (carDir);
 }
